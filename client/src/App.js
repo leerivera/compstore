a20a8d1bd1a296647bf9acb5f97328b94c6a622f
leerivera <leerivera80@icloud.com>
2021-01-20 19:59:49 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import Home from "./pages/Home"
+
+
+const App = () => {
+  return (
+
+    <Switch>
+      <Route path='/' component={Home} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+
+    </Switch>
+
+ 
+  )
+}
+
+export default App;
