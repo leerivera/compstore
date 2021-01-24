562da55f379050cadc29914a75d3fae5096077d2
leerivera <leerivera80@icloud.com>
2021-01-23 19:18:37 -0500

    created category adminroutes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/create-or-update-user", (req, res) => {
-  res.json({
-    data: "hey you hit create-or-update-user API endpoint",
-  });
-});
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
+
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
