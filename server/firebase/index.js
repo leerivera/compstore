efb8016f4dc1ce9b18cdd83ef44668e61e578d7c
leerivera <leerivera80@icloud.com>
2021-01-24 14:35:03 -0500

    product create

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -4,5 +4,6 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  atabaseURL: "https://ecommerce-11093.firebaseio.com",
 });
